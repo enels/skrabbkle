ba40f363de1cce22a13f127e511190c3be6bde8b
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-03-16 17:51:45 +0100

    define the printMenu method

--- src/skrabbkle/main/Skrabbkle.java
+++ src/skrabbkle/main/Skrabbkle.java
@@ -1,4 +1,34 @@
 package skrabbkle.main;
 
 public class Skrabbkle {
+
+    public void startGame() throws Exception{
+
+        printMenu();
+    }
+
+    private void printMenu() throws Exception{
+
+        for (int i = 0; i < 3; i++) {
+            printDash();
+            // when on second line
+            if (i == 1) {
+                System.out.print(" S k r a B B K l e ");
+
+            }
+            else {
+                printSpace();
+            }
+            printDash();
+            System.out.println();
+        } // end of for loop
+    }
+
+    private void printDash() {
+
+    }
+
+    private void printSpace() {
+
+    }
 }
