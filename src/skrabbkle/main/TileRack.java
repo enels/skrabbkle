d4aaf9fa096ceefeed9122c3e86fffe3c8ed7ca2
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-04-27 09:13:19 +0100

    define the Tiles Rack Object properties

--- src/skrabbkle/main/TileRack.java
+++ src/skrabbkle/main/TileRack.java
@@ -1,4 +1,21 @@
 package skrabbkle.main;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * The Tiles Rack Object Class
+ */
 public class TileRack {
+
+    // create the tiles rack container
+    private List<String> tilesRack = new ArrayList<String>();
+
+    // maximum number of tiles in the tiles rack
+    private int lengthOfTilesRack = 7;
+
+    // number of tiles to collect
+    private int numberOfTilesToReplace;
+
 }
