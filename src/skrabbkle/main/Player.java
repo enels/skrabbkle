fc235f5348d09b55ab94b408da79c7bbaaeb4628
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-04-27 01:38:17 +0100

    create the play method

--- src/skrabbkle/main/Player.java
+++ src/skrabbkle/main/Player.java
@@ -1,4 +1,30 @@
 package skrabbkle.main;
 
-public class Player {
+/**
+ * Player class to help both the human and computer player put tiles on the
+ * board
+ */
+public class Player extends Board{
+
+    public Player() throws Exception {
+        super();
+    }
+
+    /**
+     *
+     * @param tilesPosition stores the tiles and its position to put it on the board
+     * @return true (play successful) false (play unsuccessful)
+     */
+    public Boolean play(String [] tilesPosition) {
+        // tiles and position
+        // [0] - tiles
+        // [1] -position
+
+        // checks if it's a word in the wordlist
+        if (super.placeTilesOnPosition(tilesPosition[0], tilesPosition[1]) == 1) {
+            this.printMove(tilesPosition[0], tilesPosition[1]);
+        }
+
+        return true;
+    }
 }
