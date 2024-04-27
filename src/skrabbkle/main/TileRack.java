798386f781353e2d4ecec1a009e7fa3d48149271
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-04-27 09:30:23 +0100

    define method to poplate the tiles rack

--- src/skrabbkle/main/TileRack.java
+++ src/skrabbkle/main/TileRack.java
@@ -35,4 +35,57 @@ public class TileRack extends TileBag {
         this.numberOfTilesToReplace = nTiles;
     }
 
+    /**
+     * remove all the tiles from the tiles rack containing the word played
+     * @param wordPlayed word played by either human or computer player
+     */
+    protected void removeTilesFromTilesRack(String wordPlayed) {
+
+        for (int i = 0; i < wordPlayed.length(); i++) {
+
+            if (Character.isAlphabetic(wordPlayed.charAt(i))) {
+                int index = 0;
+                int newTilesRackSize = this.tilesRack.size();
+                int tilesRackSize = newTilesRackSize;
+                while (index < tilesRackSize && tilesRackSize == newTilesRackSize) {
+
+                    //System.out.print(" " + this.tilesRack.get(index).charAt(1));
+                    if (Character.compare(wordPlayed.charAt(i), this.tilesRack.get(index).charAt(1)) > 0) {
+                        this.tilesRack.remove(index);
+                        // get the latest size after removal
+                        newTilesRackSize = this.tilesRack.size();
+                    }
+
+                    index++;
+                }
+
+            }
+
+        }
+    }
+
+    /**
+     * Does the actual popolation of the tiles rack
+     */
+    protected void populateTilesRack() {
+
+        // set upper bound
+        int upperBound = 16;
+
+        // number of tiles to collect
+        int numberOfTilesToCollect = this.numberOfTilesToReplace;
+
+        // collect the tiles
+        for (int i = 0; i < numberOfTilesToCollect; i++) {
+
+            // randomly get tiles index to pick from tile bag
+            int tileIndex = this.generateTileIndexNumber(upperBound);
+
+            // calls method to pick tiles from the tiles bag
+            String tile = this.pickTileFromTilesBag(tileIndex);
+
+            // add to tiles Rack
+            this.addToTilesRack(tile);
+        }
+    }
 }
