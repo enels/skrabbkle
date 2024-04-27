82c65f80ec13797d98e9e2e69891a098886830e4
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-04-27 11:46:50 +0100

    declare the class attributes

--- src/skrabbkle/main/ComputerPlayer.java
+++ src/skrabbkle/main/ComputerPlayer.java
@@ -5,5 +5,24 @@ import java.util.ArrayList;
 
 public class ComputerPlayer extends Player {
 
+    //    create computer's tiles rack
+    TileRack tilesRack;
 
+    // tiles picked to be played
+    String tilesPicked = "";
+    String wordPlayed = "";
+
+    // position played
+    String position;
+
+    // tile and position played
+    String[] tilesAndPosition;
+
+    // store the score
+    private int score;
+
+    private TileBag gameTileBag;
+
+    // possible alphabets to choose from
+    String [] alphabets;
 }
